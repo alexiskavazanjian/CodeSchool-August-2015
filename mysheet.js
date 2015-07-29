64bd35cf0afc000b7f0a6c04b0d1801cdc41c44c
Aaron Crow <aaron@factual.com>
2015-07-29 09:08:30 -0700

    Update mysheet.js

--- mysheet.js
+++ mysheet.js
@@ -1,10 +1,34 @@
+/*
+ * Convenience function to get the currently active sheet
+ */
+function sheet() {
+  return SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+}
+
+function valueFromCell(cell) {
+  var rowNdx = cell.getRowIndex();
+  var colNdx = cell.getColumnIndex();
+  return sheet().getRange(rowNdx, colNdx, 1, 1).getValue();
+}
+
 function onOpen() {
   var ui = SpreadsheetApp.getUi();
   ui.createMenu('CodeSchool')
       .addItem('Hello World!', 'showDialog')
+      .addItem('Show Selected Value', 'showSelected')
       .addToUi();
 }
 
 function showDialog() {
-  SpreadsheetApp.getUi().alert('Hello World!');
+  Logger.log("saying hello...");
+  SpreadsheetApp.getUi().alert('Hello World!!');
+}
+
+function DOUBLE_IT(input) {
+  return input * 2;
+}
+
+function showSelected() {
+  var mycell = sheet().getActiveCell();
+  Browser.msgBox("vfc:" + valueFromCell(mycell));
 }
